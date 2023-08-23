6124b3daf57cf5483f77ccfd9f95975d15a8f1c4
Khrystyna <radchenko14ks@gmail.com>
2023-08-23 23:31:08 +0300

    Created a console version of To-Do List App

---
+++ Module_1/Module_1/TaskItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Module_1
+{
+    internal class TaskItem
+    {
+        public string? Task { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CompletionDate { get; set; }
+    }
+}
