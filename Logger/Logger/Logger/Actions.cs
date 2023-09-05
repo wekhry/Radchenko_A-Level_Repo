56cce44f1ce159815b49202569cf4834eea39a4e
Khrystyna <radchenko14ks@gmail.com>
2023-09-05 22:52:30 +0300

    Created Logger class with all necessary logic to collect information about the state of the application

---
+++ Logger/Logger/Logger/Actions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    internal class Actions
+    {
+        private Logger logger;
+
+        public Actions()
+        {
+            logger = Logger.GetInstance();
+        }
+
+        public Result MethodStart()
+        {
+            logger.LogInfo("Start method: MethodStart");
+            return new Result
+            {
+                Status = true
+            };
+        }
+
+        public Result MethodSkipLogic()
+        {
+            logger.LogWarning("Skipped logic in the method: MethodSkipLogic");
+            return new Result
+            {
+                Status = true
+            };
+        }
+
+        public Result MethodLogicBroke()
+        {
+            logger.LogError("I broke a logic");
+            return new Result
+            {
+                Status = false,
+                ErrorMessage = "I broke a logic"
+            };
+        }
+    }
+}
