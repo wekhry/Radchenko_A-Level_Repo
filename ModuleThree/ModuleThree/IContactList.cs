7ed373d54a40dd04d34bfe4225e6fec9a68821bc
Khrystyna <radchenko14ks@gmail.com>
2024-01-31 21:31:50 +0200

    Module three

---
+++ ModuleThree/ModuleThree/IContactList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleThree
+{
+    public interface IContactList
+    {
+        void AddContact(Contact contact);
+        List<Contact> SearchContacts(string searchTerm);
+        Task WriteToFileAsync(string filePath);
+        object FileLock { get; }
+    }
+}
---
+++ ModuleThree/ModuleThree/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModuleThree
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IContactList, ContactList>()
+                .BuildServiceProvider();
+
+            var app = new ContactListApp(serviceProvider.GetService<IContactList>());
+            app.Start();
+
+            string directoryPath = "C:\\Users\\radch\\OneDrive\\Документы\\A-Level\\GitHubRepo\\ModuleThree\\ModuleThree\\bin\\Debug\\net8.0";
+            string fileName = "contacts.txt";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            await app.ContactList.WriteToFileAsync(filePath);
+
+            var fileWatcher = new FileSystemWatcher(Path.GetDirectoryName(filePath));
+            fileWatcher.Filter = Path.GetFileName(filePath);
+            fileWatcher.Changed += async (sender, e) =>
+            {
+                while (IsFileLocked(new FileInfo(filePath)))
+                {
+                    await Task.Delay(100);
+                }
+                var contacts = File.ReadAllLines(filePath);
+                foreach (var contactData in contacts)
+                {
+                    var contactDetails = contactData.Split(',');
+                    Console.WriteLine($"Name: {contactDetails[0]}, Surname: {contactDetails[1]}, Phone:{contactDetails[2]}");
+                }
+            };
+            fileWatcher.EnableRaisingEvents = true;
+
+            Console.ReadLine();
+        }
+
+        private static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+            return false;
+        }
+    }
+}
